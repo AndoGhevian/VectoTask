a2b36fae6c72fbdea95ab04b793fe5aeeec3225c
AndoGhevian <lenyvonbeer@gmail.com>
2024-10-13 02:25:45 +0400

    modifie drag scroll hook to handle item clicks

--- src/hooks/useDraggableScroll.ts
+++ src/hooks/useDraggableScroll.ts
@@ -1,90 +1,42 @@
 import {
   RefObject,
-  useCallback,
-  useEffect,
   useMemo,
   useState,
 } from 'react'
+import { useDrag } from '@use-gesture/react'
 
-/**
- * this hook handles 2 cases:
- *   - the case when the ref has changed and the parent has re-rendered: in this case the hook state is reset
- *   - the case when the ref remains the same but the ref.current has changed: in this case the hook state is preserved
-*/
 export const useDraggableScroll = <T extends HTMLElement>(ref: RefObject<T>) => {
   const [isDragging, setIsDragging] = useState(false)
 
-  const dragScrollInfoRef = useMemo(() => ({
-    current: {
-      isDragging,
-      startX: 0,
-      scrollLeft: 0,
-    }
+  const threshold = 5
+  const isDraggingRef = useMemo(() => ({
+    current: false
   }), [ref])
 
-  const onDragScrollStart = useCallback((e: MouseEvent) => {
-    if(!e.currentTarget) {
-      return
+  useDrag(
+    ({ movement: [mx], memo = ref.current?.scrollLeft || 0, active }) => {
+      if (!active) {
+        isDraggingRef.current = false
+        setIsDragging(false)
+        return
+      }
+
+      if (!isDraggingRef.current) {
+        setIsDragging(true)
+      }
+      isDraggingRef.current = true
+
+      if (Math.abs(mx) > threshold && ref.current) {
+        ref.current.scrollLeft = memo - mx
+      }
+
+      return memo
+    },
+    {
+      target: ref,
+      filterTaps: true,
     }
-    const target = e.currentTarget as HTMLElement
-    const dragInfo = dragScrollInfoRef.current
+  )
 
-    dragInfo.isDragging = true;
-    dragInfo.startX = e.pageX - target.offsetLeft;
-    dragInfo.scrollLeft = target.scrollLeft;
-    setIsDragging(true)
-  }, [ref])
-
-  const onDragScrollEnd = useCallback((e: MouseEvent) => {
-    if(!e.currentTarget) {
-      return
-    }
-
-    const dragInfo = dragScrollInfoRef.current
-    if(!dragInfo.isDragging) {
-      return
-    }
-
-    dragInfo.isDragging = false;
-    setIsDragging(false)
-  }, [ref])
-
-  const onDragScroll = useCallback((e: MouseEvent) => {
-    if(!e.currentTarget) {
-      return
-    }
-
-    const dragInfo = dragScrollInfoRef.current
-    if (!dragInfo.isDragging) {
-      return
-    }
-    e.preventDefault();
-
-    const target = e.currentTarget as HTMLElement
-    const x = e.pageX - target.offsetLeft
-    const walkX = x - dragInfo.startX
-    target.scrollLeft = dragInfo.scrollLeft - walkX
-  }, [ref])
-
-  useEffect(() => {
-    if(!ref.current) {
-      return
-    }
-
-    const elem = ref.current
-
-    elem.addEventListener('mousedown', onDragScrollStart)
-    elem.addEventListener('mouseup', onDragScrollEnd)
-    elem.addEventListener('mouseleave', onDragScrollEnd)
-    elem.addEventListener('mousemove', onDragScroll)
-
-    return () => {
-      elem.removeEventListener('mousedown', onDragScrollStart)
-      elem.removeEventListener('mouseup', onDragScrollEnd)
-      elem.removeEventListener('mouseleave', onDragScrollEnd)
-      elem.removeEventListener('mousemove', onDragScroll)
-    }
-  })
-
-  return {isDragging}
+  return { isDragging }
 }
